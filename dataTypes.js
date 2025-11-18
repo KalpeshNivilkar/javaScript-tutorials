c7f107e01dd18d93fb24e39f62957226b6dc09b2
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-11-19 00:52:00 +0530

    this is a file of object

---
+++ dataTypes.js
@@ -0,0 +1,14 @@
+let name = "kalpesh"
+console.log(name, typeof(name));
+
+let age = 22
+console.log(age, typeof(age));
+
+let isEmpty = false
+console.log(isEmpty, typeof(isEmpty));
+
+let x;
+console.log(x,typeof(x));
+
+let y = null
+console.log(y, typeof(y));
