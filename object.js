f42e975fa482928b8622256b09268498c47505ad
KalpeshNivilkar <kalpeshnivilkar4@gmail.com>
2025-11-26 23:11:09 +0530

    add new file for arithmetic operator

---
+++ object.js
@@ -0,0 +1,24 @@
+const student = {
+    name : "kalpesh",
+    add : "pune",
+    rollNo : 101,
+    isPass : true
+};
+
+console.log(student);
+
+student.name = "vikas";
+console.log(student.name);
+console.log(student);
+
+// practice
+
+const product = {
+    name : "pen",
+    rating : 4,
+    isDeal : true,
+    price : 230,
+    offer : "5%"
+};
+
+console.log(product);
